a5f5d323e82535f1b9466ba2f271dbe11e62b843
Jesus Cabrera <jcabrera@desarrollo01.mppct.gob.ve>
2020-06-17 20:54:26 +0000

    Last changes frontend, add fonts and logos

--- frontend/src/components/AppFrame/index.js
+++ frontend/src/components/AppFrame/index.js
@@ -6,10 +6,16 @@ import { Helmet } from "react-helmet"
 import Header from 'components/Header'
 import Footer from 'components/Footer'
 
+// @Statics > Icon
+import Logo from 'statics/images/logos/juventud.png'
+
 function index({ title = "Inicio", children } = {}) {
     return (
         <div className="app--frame">
-            <Helmet> <title>{title} | Semillero</title> </Helmet>
+            <Helmet> 
+                <link rel="icon" href={Logo} type="image/gif" sizes="16x16" />
+                <title>{title} | CRS</title> 
+            </Helmet>
             <Header />
             <main role="main">
                 {children}
